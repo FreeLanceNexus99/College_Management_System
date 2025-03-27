a67d39d84ede4b51ca477d58c10d6bb4798e61bc
jobin <jobinjose703@gmail.com>
2025-03-27 22:38:56 +0530

    added a new admit form for students in collage admin appp template

--- college_management/urls.py
+++ college_management/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),  # Authentication URLs
-    path('college_admin/', include('college_admin.urls')),  # Admin Dashboard
+    path('college_admin/', include('college_admin.urls',namespace='college_admin')),  # Admin Dashboard
     path('teachers/', include('teachers.urls')),  # Teacher Dashboard
     path('students/', include('students.urls')),  # Student Dashboard
     path('library/', include('library.urls')),  # Library Dashboard
